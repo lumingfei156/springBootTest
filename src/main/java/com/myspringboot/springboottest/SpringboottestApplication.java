5983b4d688400de43937f9715030278f21fc5cb4
李舜生 <33613049+lumingfei156@users.noreply.github.com>
2018-12-03 15:31:50 +0800

    修改了启动类 1 添加了controller的扫描路径 2 删除了启动类中的一个controller

--- src/main/java/com/myspringboot/springboottest/SpringboottestApplication.java
+++ src/main/java/com/myspringboot/springboottest/SpringboottestApplication.java
@@ -2,6 +2,7 @@ package com.myspringboot.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,20 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2018/12/3
  * springBoot的启动类
  */
-@RestController
 @SpringBootApplication
+@ComponentScan("com.springboot.*")
 public class SpringboottestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottestApplication.class, args);
 	}
-
-	/**
-	 * 测试是否启动成功
-	 * @return hello world springBoot
-	 */
-	@RequestMapping("/")
-	public String index(){
-		return "Hello world springBoot";
-	}
+	
 }

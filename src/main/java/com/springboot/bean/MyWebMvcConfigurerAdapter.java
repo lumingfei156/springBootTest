d15ea48cb67bb2d371a90b6c6e343321ceaba84a
李舜生 <33613049+lumingfei156@users.noreply.github.com>
2018-12-04 14:50:10 +0800

    不需要通过controller直接访问html文件

--- src/main/java/com/springboot/bean/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/springboot/bean/MyWebMvcConfigurerAdapter.java
@@ -2,6 +2,7 @@ package com.springboot.bean;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -12,10 +13,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
-
+    /**
+     * 自定义静态文件存放位置
+     * @param registry ss
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
         super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 将某一访问路径和某个页面绑定，即不通过controller直接访问某个页面
+     * @param registry ss
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/goHelloWorld").setViewName("HelloWorld");
+        super.addViewControllers(registry);
     }
 }
